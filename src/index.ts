64eaf33827e1b39387c73b1f91d999d9c0208e65
JacobRWebb <xxpwnage37xx@gmail.com>
2022-03-24 14:55:31 -0500

    Created Socket Server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import Express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = Express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+  transports: ["websocket"],
+});
 const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
+
+io;
 
 app.use("*", (req, res) => {
   res.send("API Endpoint");
